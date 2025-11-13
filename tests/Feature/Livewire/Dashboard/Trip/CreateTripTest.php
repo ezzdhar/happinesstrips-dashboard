8ea59be8964fec85046c8ce84dd6f7da88e4439f
Essam Hamdy AL-agamy <20912018100106@fci.zu.edu.eg>
2025-11-13 12:46:56 +0200

    Add print functionality for hotel bookings, including a dedicated print view and related language translations

--- tests/Feature/Livewire/Dashboard/Trip/CreateTripTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Livewire\Dashboard\Trip\CreateTrip;
-use Livewire\Livewire;
-
-it('renders successfully', function () {
-    Livewire::test(CreateTrip::class)
-        ->assertStatus(200);
-});
--- tests/Feature/Livewire/Dashboard/Trip/TripDataTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Livewire\Dashboard\Trip\TripData;
-use Livewire\Livewire;
-
-it('renders successfully', function () {
-    Livewire::test(TripData::class)
-        ->assertStatus(200);
-});
